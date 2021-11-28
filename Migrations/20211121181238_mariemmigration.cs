74b93dc0f70a0ce38847697240bc7abac82bd576
smii <mariem.smii@enis.tn>
2021-11-28 11:23:24 +0100

    templatepush

---
+++ Migrations/20211121181238_mariemmigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eEnchere.Migrations
+{
+    public partial class mariemmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20211126104201_base.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eEnchere.Migrations
+{
+    public partial class @base : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
